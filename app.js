ac0855e51f7ba3c59e4c966c03c4e7d92292d1b7
Grace <mgracebodur@gmail.com>
2019-12-19 21:34:13 -0500

    Co-authored-by: malekadair <malekadair@gmail.com>

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const validMovies = require('./moviedex.json')
+const cors = require('cors')
+const helmet = require('helmet')
+
+
+//authorization header X
+//make token secret X
+//validate X
+//middleware x
+//filtering
+//cors, helmet
+
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
 
-// app.use((req, res) => {
-//     res.send('Hello Grace!')
-// })
+app.use(function authorize(req,res,next){
+    const TOKEN = process.env.API_TOKEN
+    const token = req.header('Authorization').split(' ')[1]
 
-function handleGetMovie(req, res) {
-    res.json(validMovie)
-}
 
-app.get('/movie', handleGetMovie)
+    if(token === TOKEN){
+        next()
+    }else {
+        res.sendStatus(401)
+    }
 
-const validMovie = require('./moviedex.json')
+})
+
+app.get('/movie', function handleGetMovies(req, res) {
+    let response = validMovies
+    if (req.query.genre){
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        )
+    }
+    if (req.query.country){
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        )
+    }
+    if (req.query.avg_vote){
+        response = response.filter(movie =>
+            Number(movie.avg_vote) >= Number(req.query.avg_vote)
+        )
+    }
+    res.json(response)
+})
+
+
+
 
 module.exports = app
